a96629215f8428eee3da3c85fad84e339b09a981
httpedor <httpedor.dev@gmail.com>
2022-09-02 14:37:11 -0300

    first commit

--- Makefile
+++
@@ -1,4 +0,0 @@
-HTML:
-	em++ -o out/main.js main.cpp -Os -I ./include -Wall lib/libraylib.a -s USE_GLFW=3 -s ASSERTIONS=1 -s SINGLE_FILE -g
-WIN32:
-	g++ -o out/main.exe main.cpp -Os -Wall -g ./lib/raylibdll.lib -I ./include
--- out/resizer.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("resize", () =>{
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    var canvas = document.getElementById("canvas");
-    canvas.width = w;
-    canvas.height = h;
-});
